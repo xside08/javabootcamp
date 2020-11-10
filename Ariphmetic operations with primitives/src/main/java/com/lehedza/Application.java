4731a8e2d1467f04857a071f5999d7b59a36b34f
xside <xside08@gmail.com>
2020-11-10 19:39:17 +0200

    changed operational methods with primitives

--- Ariphmetic operations with primitives/src/main/java/com/lehedza/Application.java
+++ Ariphmetic operations with primitives/src/main/java/com/lehedza/Application.java
@@ -2,15 +2,25 @@ package main.java.com.lehedza;
 
 import main.java.com.lehedza.primitives.Mathematic;
 
+import java.util.Scanner;
+
 public class Application {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        System.out.println("Enter first number: ");
+        double firstNum = sc.nextDouble();
+        System.out.println("Enter second number: ");
+        double secondNum = sc.nextDouble();
+
         Mathematic m = new Mathematic();
-//        System.out.println(m.getSumOfInt(3, 5));
-//        System.out.println(m.getSumOfDouble(4.2, 5d));
-//        System.out.println("your result is : " + m.resultOfDivide());
-//        System.out.println("hello " + m.resultOfIncrease() + " times!");
-        System.out.println(m.subtractResult());
+        System.out.println("Your result of increase is : " + m.getResultOfIncrease(firstNum, secondNum));
+        System.out.println("Your result of substract is : " + m.getResOfSusbstract(firstNum, secondNum));
+        System.out.println("Your result of divide is : " + m.getResultOfDivide(firstNum, secondNum));
+        System.out.println("Your result of sum is : " + m.getResultOfSum(firstNum, secondNum));
+        double res = m.getSumIntDouble(4, 3.9);
+        System.out.println(res);
 
     }
 }
--- Ariphmetic operations with primitives/src/main/java/com/lehedza/primitives/Mathematic.java
+++ Ariphmetic operations with primitives/src/main/java/com/lehedza/primitives/Mathematic.java
@@ -1,48 +1,30 @@
 package main.java.com.lehedza.primitives;
 
-import java.util.Scanner;
 
 public class Mathematic {
-    Scanner sc = new Scanner(System.in);
-
-    public int getSumOfInt(int a, int b) {
-        return a + b;
-    }
-
-    public double getSumOfDouble(double a, double b) {
-        return a + b;
-    }
 
     public double getSumIntDouble(int a, double b) {
         return a + b;
     }
 
-    public double getSumDoubleFloat(double a, float b) {
-        return a + b;
-    }
-
-    public int getSumByteShort(byte a, short b) {
-        return a + b;
-    }
-
-    public int getSumByteLong(byte a, long b) {
-        return (int) (a + b);
-    }
-
-    public int resultOfDivide() {
-        int res = sc.nextInt() / sc.nextInt();
+    public double getResultOfSum(double firstNum, double secondNum) {
+        double res = firstNum + secondNum;
         return res;
     }
 
-    public double resultOfIncrease() {
-        double res = (sc.nextDouble() * sc.nextInt());
+    public double getResultOfIncrease(double firstNum, double secondNum) {
+        double res = firstNum * secondNum;
         return res;
     }
 
-    public int subtractResult() {
-        int res = sc.nextInt() - sc.nextInt();
+    public double getResultOfDivide(double firstNum, double secondNum) {
+        double res = firstNum / secondNum;
         return res;
+    }
 
+    public double getResOfSusbstract(double firstNum, double secondNum) {
+        double res = firstNum - secondNum;
+        return res;
     }
 
 }
